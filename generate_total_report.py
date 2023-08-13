1f80a2e35fcdd377130521b1fa16826dd81c1a06
igorshishkin96 <117339507+igorshishkin96@users.noreply.github.com>
2023-08-13 06:20:02 +0500

    Update generate_total_report.py

--- generate_total_report.py
+++ generate_total_report.py
@@ -40,53 +40,53 @@ def generate_report(answer, start_date, end_date):
         # Вычисляем общую прибыль
         total_profit += front_total_price
 
-        # Создаем новый файл Excel
-        workbook = openpyxl.Workbook()
-        sheet = workbook.active
-        sheet.title = 'Общий отчет'
+    # Создаем новый файл Excel
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    sheet.title = 'Общий отчет'
 
-        # Заполняем заголовки столбцов
-        headers = ['Показатель', 'Значение']
-        sheet.append(headers)
+    # Заполняем заголовки столбцов
+    headers = ['Показатель', 'Значение']
+    sheet.append(headers)
 
-        # Заполняем таблицу данными
-        report_data = [
-            ('Дата первой продажи', first_sale_date.strftime('%Y-%m-%d %H:%M:%S') if first_sale_date else ''),
-            ('Дата последней продажи', last_sale_date.strftime('%Y-%m-%d %H:%M:%S') if last_sale_date else ''),
-            ('Сумма скидок', total_discount),
-            ('Сумма надбавок', total_surcharge),
-            ('Количество отмен', total_canceled),
-            ('Общая прибыль', total_profit)
-        ]
+    # Заполняем таблицу данными
+    report_data = [
+        ('Дата первой продажи', first_sale_date.strftime('%Y-%m-%d %H:%M:%S') if first_sale_date else ''),
+        ('Дата последней продажи', last_sale_date.strftime('%Y-%m-%d %H:%M:%S') if last_sale_date else ''),
+        ('Сумма скидок', total_discount),
+        ('Сумма надбавок', total_surcharge),
+        ('Количество отмен', total_canceled),
+        ('Общая прибыль', total_profit)
+    ]
 
-        for item in report_data:
-            sheet.append(item)
+    for item in report_data:
+        sheet.append(item)
 
-        # Настройка границ ячеек и выравнивания текста
-        thin_border = Border(left=Side(style='thin'),
-                             right=Side(style='thin'),
-                             top=Side(style='thin'),
-                             bottom=Side(style='thin'))
+    # Настройка границ ячеек и выравнивания текста
+    thin_border = Border(left=Side(style='thin'),
+                         right=Side(style='thin'),
+                         top=Side(style='thin'),
+                         bottom=Side(style='thin'))
 
-        header_left_alignment = Alignment(horizontal='left', vertical='center')
-        value_right_alignment = Alignment(horizontal='right', vertical='center')
+    header_left_alignment = Alignment(horizontal='left', vertical='center')
+    value_right_alignment = Alignment(horizontal='right', vertical='center')
 
-        for row in sheet.iter_rows(min_row=1, max_row=len(report_data) + 1, max_col=2):
-            header_cell, value_cell = row
-            header_cell.border = thin_border
-            value_cell.border = thin_border
+    for row in sheet.iter_rows(min_row=1, max_row=len(report_data) + 1, max_col=2):
+        header_cell, value_cell = row
+        header_cell.border = thin_border
+        value_cell.border = thin_border
 
-            header_cell.alignment = header_left_alignment
-            value_cell.alignment = value_right_alignment
+        header_cell.alignment = header_left_alignment
+        value_cell.alignment = value_right_alignment
 
-        for column_cells in sheet.columns:
-            max_length = max(len(str(cell.value)) for cell in column_cells)
-            adjusted_width = (max_length + 2) * 1.2
-            sheet.column_dimensions[column_cells[0].column_letter].width = adjusted_width
+    for column_cells in sheet.columns:
+        max_length = max(len(str(cell.value)) for cell in column_cells)
+        adjusted_width = (max_length + 2) * 1.2
+        sheet.column_dimensions[column_cells[0].column_letter].width = adjusted_width
 
-        # Сохраняем файл Excel с уникальным именем, включающим дату
-        excel_filename = f'total_report_{start_date}_{end_date}.xlsx'
-        workbook.save(excel_filename)
+    # Сохраняем файл Excel с уникальным именем, включающим дату
+    excel_filename = f'total_report_{start_date}_{end_date}.xlsx'
+    workbook.save(excel_filename)
 
     # Проверяем наличие файла перед открытием
     try:
